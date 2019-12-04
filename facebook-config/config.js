71b0f57760da3fc140f4e764e91bdc54cbd803b7
Vitali Brych <vbrych@cpaglobal.com>
2019-12-04 12:33:50 +0300

    refactore: delete logs

--- facebook-config/config.js
+++ facebook-config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'appID' : '565056977603490',
-    'appSecret' : '039f22840c5a6ec52c87ca6bd5297fe3',
-    'callbackUrl' : 'http://localhost:5500/auth/facebook/callback'
+    'appID' : myAppID,
+    'appSecret' : myAppSecret,
+    'callbackUrl' : myCallbackUrl
 };
